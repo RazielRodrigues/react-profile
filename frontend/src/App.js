f75fe480849cf9afee6db4b102a3bdd798cea5e6
razielrodrigues <raziel@upoutsourcing.com>
2022-05-25 21:34:29 -0300

    wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   useEffect(() => {
     setAppState({ loading: true });
-    const apiUrl = 'https://api.github.com/users/hacktivist123/repos';
+    const apiUrl = 'https://api.github.com/users/RazielMiranda/repos';
     axios.get(apiUrl).then((repos) => {
       const allRepos = repos.data;
       setAppState({ loading: false, repos: allRepos });
