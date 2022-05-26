aa56d996857f6b708b3ec90c35350ce3c704d27c
razielrodrigues <raziel@upoutsourcing.com>
2022-05-25 21:10:29 -0300

    adding code to the root and css

---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import List from './components/List';
+import withListLoading from './components/withListLoading';
+function App() {
+  const ListLoading = withListLoading(List);
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `https://api.github.com/users/RazielMiranda/repos`;
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((repos) => {
+        setAppState({ loading: false, repos: repos });
+      });
+  }, [setAppState]);
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1>My Repositories</h1>
+      </div>
+      <div className='repo-container'>
+        <ListLoading isLoading={appState.loading} repos={appState.repos} />
+      </div>
+      <footer>
+        <div className='footer'>
+          Built{' '}
+          <span role='img' aria-label='love'>
+            💚
+          </span>{' '}
+          with by Raziel Rodrigues
+        </div>
+      </footer>
+    </div>
+  );
+}
+export default App;
