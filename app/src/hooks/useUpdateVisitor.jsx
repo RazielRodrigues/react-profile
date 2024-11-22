4d7e2a5f09e380b5a36510717487ccd2b629754f
Raziel Rodrigues <razielx3@live.com>
2024-11-22 12:05:14 +0000

    fixing set cookie

--- app/src/hooks/useUpdateVisitor.jsx
+++ app/src/hooks/useUpdateVisitor.jsx
@@ -1,13 +1,33 @@
 import { doc, updateDoc, increment, getDoc } from "firebase/firestore";
 import FirebaseConfig from "../FirebaseConfig";
 
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
 export default async function useUpdateVisitor() {
     const db = FirebaseConfig();
     const docId = "MY6UiE8cfjsZ4M2VUsD4";
     const docRef = doc(db, "visitor", docId);
 
     try {
-        if (document.cookie) {
+        if (getCookie('visited')) {
             return;
         }
 
@@ -22,7 +42,7 @@ export default async function useUpdateVisitor() {
             await setDoc(docRef, { visitor_count: 1 });
         }
 
-        document.cookie = 'already-visited'
+        setCookie('visited', 'true', 360)
     } catch (error) {
         console.error("Error updating visitor count:", error);
     }
